5692d40317862dba3a319e352dcd468cc337d454
Joodi <winbeta.com@gmail.com>
2025-09-14 23:16:33 +0330

    up

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,6 +1,10 @@
 import SingleProject from "@/components/SingleProject/SingleProject";
 
-const Page = ({ params }: { params: { id: string } }) => {
+interface PageProps {
+  params: { id: string };
+}
+
+const Page = async ({ params }: PageProps) => {
   return <SingleProject projectId={Number(params.id)} />;
 };
 
